e1562392eff7ec6916bbdf4bd800da1527f63dc1
andreassosilo <andreas.sosilo@gmail.com>
2019-09-13 13:42:51 +0700

    Add MongoDB ATLAS_CONNECT

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV == 'development') {
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const mongoose = require('mongoose')
 const cors = require('cors')
 
 app.use(cors())
@@ -14,6 +14,13 @@ app.use(express.json())
 app.use(express.urlencoded({
   extended: false
 }))
+
+mongoose.connect(process.env.ATLAS_CONNECT, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log('Connected to MongoDB database')
+  }).catch((err) => {
+    console.log(err, 'Could not connected to MongoDB database')
+  })
 
 const post = require('./routes/post')
 const errorHandler = require('./middlewares/errHandler')
--- routes/post.js
+++ routes/post.js
@@ -4,9 +4,5 @@ const upload = require('../middlewares/gcs')
 
 router.post('/', upload.multer.single('audio'), upload.sendUploadToGCS, upload.validate_format, postModel.create)
 router.get('/', postModel.findAll)
-router.get('/test', (req, res, next) => {
-  res.send('hello world!')
-})
-
 
 module.exports = router
